5377f1e4edccebc0dc7b9b20230a6cda73a06023
Vidit Gupta <viditgupta721@gmail.com>
2025-11-26 03:42:10 +0530

    Initial commit for Splitlio Beta Version

---
+++ README.md
@@ -0,0 +1,25 @@
+# Full Stack AI Splitwise Clone with Next JS, Convex, Tailwind, Inngest, Shadcn UI Tutorial 🔥🔥
+## https://youtu.be/Ce7O3p7-YDI
+
+![splitr](https://github.com/user-attachments/assets/11e138c4-efcf-4a85-8586-f2993da118d8)
+
+### Make sure to create a `.env` file with following variables -
+
+```
+# Deployment used by `npx convex dev`
+CONVEX_DEPLOYMENT=
+
+NEXT_PUBLIC_CONVEX_URL=
+
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=
+CLERK_SECRET_KEY=
+
+NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
+NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+
+CLERK_JWT_ISSUER_DOMAIN=
+
+RESEND_API_KEY=
+
+GEMINI_API_KEY=
+```
---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,65 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as contacts from "../contacts.js";
+import type * as dashboard from "../dashboard.js";
+import type * as email from "../email.js";
+import type * as expenses from "../expenses.js";
+import type * as groups from "../groups.js";
+import type * as inngest from "../inngest.js";
+import type * as seed from "../seed.js";
+import type * as settlements from "../settlements.js";
+import type * as users from "../users.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  contacts: typeof contacts;
+  dashboard: typeof dashboard;
+  email: typeof email;
+  expenses: typeof expenses;
+  groups: typeof groups;
+  inngest: typeof inngest;
+  seed: typeof seed;
+  settlements: typeof settlements;
+  users: typeof users;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
